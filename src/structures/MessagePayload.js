9bb3c39f02ad329fa1d1448d8f01e1705417e622
nab138 <56705177+nab138@users.noreply.github.com>
2021-08-06 15:35:49 -0400

    Update MessagePayload.js

--- src/structures/MessagePayload.js
+++ src/structures/MessagePayload.js
@@ -182,7 +182,7 @@ class MessagePayload {
       content,
       tts,
       nonce,
-      embeds: this.options instanceof MessageEmbed ? [ this.options ] : [],
+      embeds: typeof this.Options === "object" ? [ this.options ] : [],
       components,
       username,
       avatar_url: avatarURL,
